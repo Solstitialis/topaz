f35194e8aa2d9b3b7d47e242a790f428f4cf4920
Solstitialis <72421763+Solstitialis@users.noreply.github.com>
2020-10-08 21:48:30 -0600

    Add esuna, heal, and levelsyncoff commands

---
+++ scripts/commands/heal.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------------------------------------
+-- func: heal
+-- desc: Restores all HP and MP to the player
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 2,
+    parameters = ""
+};
+
+function onTrigger(player)
+    if (player:getHP() > 0) then
+        player:setHP(9999);
+        player:setMP(9999);
+    end
+end
