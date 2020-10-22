72b65fd3448ff0b9f7988576f75c5d3049098512
Solstitialis <72421763+Solstitialis@users.noreply.github.com>
2020-10-22 10:04:00 -0600

    Increase Phalanx resistance at GM level
    Add PHALANX_I_DURATION and PHALANX_II_DURATION to settings.lua. Change default from 180 and 240 to 1800.
    Change phalanx.lua and phalanx_ii.lua to use PHALANX_I_DURATION and PHALANX_II_DURATION.
    Add GMLEVEL_PHALANX_MULTIPLIER and PHALANX_MULTIPLIER to settings.lua.
    Change phalanx.lua and phalanx_ii to apply resistance multiplier at GM level.

--- scripts/globals/spells/phalanx_ii.lua
+++ scripts/globals/spells/phalanx_ii.lua
@@ -13,7 +13,7 @@ end
 function onSpellCast(caster, target, spell)
     local enhskill = caster:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
     local final = 0
-    local duration = calculateDuration(240, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local duration = calculateDuration(PHALANX_II_DURATION, spell:getSkillType(), spell:getSpellGroup(), caster, target)
     duration = calculateDurationForLvl(duration, 75, target:getMainLvl())
 
     if enhskill <= 300 then
@@ -21,6 +21,13 @@ function onSpellCast(caster, target, spell)
     else
         final = math.floor((enhskill - 300.5) / 28.5) + 28
     end
+
+    -- Apply resistance multiplier at GM level
+    --local before = final
+    if(caster:getObjType() == tpz.objType.PC and caster:getGMLevel() >= GMLEVEL_PHALANX_MULTIPLIER and target:getObjType() == tpz.objType.PC and target:getGMLevel() >= GMLEVEL_PHALANX_MULTIPLIER) then
+        final = final * PHALANX_MULTIPLIER
+    end
+    --caster:PrintToPlayer(string.format("Phalanx (Before,After): %s,%s", before, final))
 
     if target:addStatusEffect(tpz.effect.PHALANX, final, 0, duration) then
         spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
