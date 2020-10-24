24c2ae180f0119a6a01fa147d116b0c4f2177c34
Solstitialis <72421763+Solstitialis@users.noreply.github.com>
2020-10-24 12:08:56 -0600

    Add sneak.lua command to give Invisible, Sneak, and Deodorize with infinite duration

---
+++ scripts/commands/sneak.lua
@@ -0,0 +1,16 @@
+---------------------------------------------------------------------------------------------------
+-- func: sneak
+-- desc: Adds Invisible, Sneak, and Deodorize with infinite duration to the player.
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = ""
+}
+
+function onTrigger(player)
+    player:addStatusEffect(tpz.effect.INVISIBLE, 1, 0, 0)
+    player:addStatusEffect(tpz.effect.SNEAK, 1, 0, 0)
+    player:addStatusEffect(tpz.effect.DEODORIZE, 1, 0, 0)
+end
