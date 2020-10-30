ed6eb1291e719a701ea830f33e7b4c45769af8d7
Solstitialis <72421763+Solstitialis@users.noreply.github.com>
2020-10-29 20:04:54 -0600

    Change heal.lua to also heal pet

--- scripts/commands/heal.lua
+++ scripts/commands/heal.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------------------------------
 -- func: heal
--- desc: Restores all HP and MP to the player
+-- desc: Restores all HP and MP to the player and pet
 ---------------------------------------------------------------------------------------------------
 
 cmdprops =
@@ -13,5 +13,11 @@ function onTrigger(player)
     if (player:getHP() > 0) then
         player:setHP(9999);
         player:setMP(9999);
+
+        local pet = player:getPet()
+        if pet then
+            pet:setHP(9999)
+            pet:setMP(9999)
+        end
     end
 end
